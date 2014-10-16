e170962bdb3ea2e026915fd612778311d1e1dc95
rudyjason <rudyjason.ep@hotmail.com>
2014-10-16 14:09:19 +0200

    lots of shit

--- FasterMindC/FasterMindC/FM_Client_Controller.cs
+++ FasterMindC/FasterMindC/FM_Client_Controller.cs
@@ -1,28 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FasterMindC
 {
-    class FM_Client_Controller
+    public class FM_Client_Controller
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
+
+        private string _name { get; set; }
+        private short _code { get; set; }
+
         static void Main()
         {
+            FM_Client_Controller control = new FM_Client_Controller();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FM_Client_GUI());
-            new FM_Client_Controller();
+            Application.Run(new FM_Client_GUI(control));
         }
 
         public FM_Client_Controller()
         {
+            this._code = 0;
+        }
 
+        public void NameButtonClick(object sender, EventArgs e, string name)
+        {
+            this._name = name;
+        }
+
+        internal void InputCodeClicked(object sender, EventArgs e, int p)
+        {
+            if ((_code / Math.Pow(1, p - 1)) == 6)
+            {
+
+            }
+            else
+            {
+                _code += (short)(Math.Pow(1, p - 1));
+            }
+        }
+
+        internal void SubmitButtonClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- FasterMindC/FasterMindC/FM_Client_GUI.cs
+++ FasterMindC/FasterMindC/FM_Client_GUI.cs
@@ -12,19 +12,42 @@ namespace FasterMindC
 {
     public partial class FM_Client_GUI : Form
     {
-        public FM_Client_GUI()
+        FM_Client_Controller _controller;
+        public FM_Client_GUI(FM_Client_Controller controller)
         {
+            this._controller = controller;
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void NameButtonClicked(object sender, EventArgs e)
         {
-
+            PlayerName.Text = nameBox.Text;
+            _controller.NameButtonClick(sender, e, PlayerName.Text);
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void SubmitButtonClicked(object sender, EventArgs e)
         {
+            _controller.SubmitButtonClicked(sender, e);
+        }
 
+        private void InputCode1Clicked(object sender, EventArgs e)
+        {
+            _controller.InputCodeClicked(sender, e, 1);
+        }
+
+        private void InputCode2Clicked(object sender, EventArgs e)
+        {
+            _controller.InputCodeClicked(sender, e, 2);
+        }
+
+        private void InputCode3Clicked(object sender, EventArgs e)
+        {
+            _controller.InputCodeClicked(sender, e, 3);
+        }
+
+        private void InputCode4Clicked(object sender, EventArgs e)
+        {
+            _controller.InputCodeClicked(sender, e, 4);
         }
     }
 }
