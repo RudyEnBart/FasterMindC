66356d769608c71d5b33a2f4b65417d772e66b49
rudyjason <rudyjason.ep@hotmail.com>
2014-10-16 11:00:40 +0200

    network start

---
+++ FasterMindC/ClassLibrary1/FM_Packet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMNetworkLibrary
+{
+    public class FM_Packet
+    {
+        public string _id { get; set; }
+        public string _type { get; set; }
+        public string _message { get; set; }
+
+        public FM_Packet(string id, string type, string message)
+        {
+            this._id = id;
+            this._type = type;
+            this._message = message;
+        }
+
+        public string toString()
+        {
+            return "ID: " + _id + " Type: " + _type + " \nMessage: " + _message; 
+        }
+    }
+}
--- FasterMindC/Server/FM_Server.cs
+++ FasterMindC/Server/FM_Server.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Server
 {
-    static class FM_Server
+    class FM_Server
     {
         /// <summary>
         /// The main entry point for the application.
@@ -14,6 +17,56 @@ namespace Server
         [STAThread]
         static void Main()
         {
+            new FM_Server();
+        }
+
+        public FM_Server()
+        {
+            TcpListener server = new TcpListener(_localIP, 11000);
+            server.Start();
+
+            TcpClient incomingClient;
+            Console.WriteLine("Waiting for connection...");
+            while (true)
+            {
+                incomingClient = server.AcceptTcpClient();
+
+                new Thread(() =>
+                {
+                    Console.WriteLine("Connection found!");
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    TcpClient sender = incomingClient;
+                    SslStream sslStream = new SslStream(incomingClient.GetStream());
+                    sslStream.AuthenticateAsServer(certificate);
+
+                    while (true)
+                    {
+
+                        String dataString = "";
+                        FM_Packet packet = null;
+                        if (sender.Connected)
+                        {
+                            dataString = (String)formatter.Deserialize(sslStream);
+                            //dataString = (String)formatter.Deserialize(sender.GetStream());
+                            packet = Utils.GetPacket(dataString);
+
+                            //Console.WriteLine(dataString);
+
+                            //Console.WriteLine("Incoming action" + packet._type);
+                            switch (packet._type)
+                            {
+                                //sender = incoming client
+                                //packet = data van de client
+                                case "Chat":
+                                    HandleChatPacket(packet);
+                                    break;
+                                default: //nothing
+                                    break;
+                            }
+                        }
+                    } // end While
+                }).Start();
+            }
 
         }
     }
