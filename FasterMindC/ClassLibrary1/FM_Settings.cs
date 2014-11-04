1545b765ba62465ba0069224e04d1db7f665f9d7
rudyjason <rudyjason.ep@hotmail.com>
2014-11-04 12:44:37 +0100

    Bart is server

--- FasterMindC/ClassLibrary1/FM_Settings.cs
+++ FasterMindC/ClassLibrary1/FM_Settings.cs
@@ -8,8 +8,9 @@ namespace FMNetworkLibrary
 {
     public static class FM_Settings
     {
+        public const string SERVERIP = "145.102.79.74";
         //public const string SERVERIP = "192.168.1.114";
-        public const string SERVERIP = "127.0.0.1";
+        //public const string SERVERIP = "127.0.0.1";
         //public const string SERVERIP = "81.68.116.118";
 
         public const byte SERVERPORT = 42;
